bacdb56f5b8c35e7006a95ee8f40c135117a6a2b
mekkiouertani <mekki.ouertani@gmail.com>
2023-10-12 18:43:03 +0200

    add button reset, add bg-color to body

--- js/script.js
+++ js/script.js
@@ -10,39 +10,44 @@ const emailVerified = [
     "boolean@outlook.it"
 ];
 
-//prendiamo il bottone invio
+//prendiamo il bottone invio e reset
 const button = document.getElementById('btn-submit');
+const buttonRst = document.getElementById('btn-reset');
 //aggiungiamo una variabile di controllo
 let founded = false;
 //prendiamo il div dove inseriremo la risposta della verificazione
 const boxAnswer = document.querySelector('.answer');
 
 //aggiungiamo eventListener
-button.addEventListener('click',
-        function(){
-        //prendiamo la mail che viene inserita nell'input
-        emailUser = document.getElementById('user-email').value;
-        console.log(emailUser);
+button.addEventListener('click',function(){
+    //prendiamo la mail che viene inserita nell'input
+    emailUser = document.getElementById('user-email').value;
+    console.log(emailUser);
 
-        //aggiungiamo ciclo For per constare se la mail è nella lista
-        for (let i = 0; i < emailVerified.length; i++) {
-            let emailToVerified = emailVerified[i];
-            if (emailToVerified === emailUser){
-                founded = true;
-                console.log(emailToVerified);
-            }
-        } 
-
-        //verifichiamo che la mail sia registrata nel sistema e stampiamo il messaggio
-        if(founded) {
-            boxAnswer.innerHTML = `Login effettuato`;
-        }   else    {
-            boxAnswer.innerHTML = `Email non registrata`;
+    //aggiungiamo ciclo For per constare se la mail è nella lista
+    for (let i = 0; i < emailVerified.length; i++) {
+        let emailToVerified = emailVerified[i];
+        if (emailToVerified === emailUser){
+            founded = true;
+            console.log(emailToVerified);
         }
+    } 
+
+    //verifichiamo che la mail sia registrata nel sistema e stampiamo il messaggio
+    if(founded) {
+        boxAnswer.innerHTML = `Login effettuato`;
+    }   else    {
+        boxAnswer.innerHTML = `Inserisci una email valida <br> prova con <em>"boolean@outlook.com"</em>`;
     }
-)
+})
+
+//aggiungiamo funzione per il reset button
+buttonRst.addEventListener('click', function(){
+    let resetTextEmail = document.getElementById('user-email').value= '';
+})
 
 //consoleLOG
 console.log(button);
+console.log(buttonRst);
 console.log(emailVerified);
 
