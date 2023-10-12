53ce0fa820c885498c4a00845703ebfcff3bf152
mekkiouertani <mekki.ouertani@gmail.com>
2023-10-12 15:58:41 +0200

    added condition to verify email

--- js/script.js
+++ js/script.js
@@ -9,22 +9,37 @@
 
 
 //mail
+  //variabili e constanti
     //inseriamo le email già registrate nel sistema
 const emailVerified = ["ciao@gmail.com", "boolean@outlook.it", "mekki@gmail.com"];
     //prendiamo il bottone invio
 const button = document.getElementById('btn-submit');
+    //aggiungiamo una variabile di controllo
+let founded = false;
     //aggiungiamo eventListener
 button.addEventListener('click',
         function(){
             //prendiamo la mail che viene inserita nell'input
         emailUser = document.getElementById('user-email').value;
                 console.log(emailUser);
+            //aggiungiamo ciclo For per constare se la mail è nella lista
+        for (let i = 0; i < emailVerified.lenght; i++); {
+                let emailToVerified = emailVerified[1];
+                if (emailToVerified === emailUser){
+                founded = true
+                console.log(emailToVerified);
+                }
+        }  //verifichiamo che la mail sia registrata nel sistema
+        if(founded){
+            const boxAnswer = document.querySelector('.answer');
+            boxAnswer.innerHTML = `Login effettuato`
+        }   else{
+            const boxAnswer = document.querySelector('.answer');
+            boxAnswer.innerHTML = `Email non registrata`;
+        }
+
         }
 )
-
-
-
-
 
 
 
